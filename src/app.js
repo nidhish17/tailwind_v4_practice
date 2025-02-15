735a8fc000c4f19c195d0b16544f27d133aaee6f
nidhish <nidhish1233@gmail.com>
2025-02-15 13:54:10 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const darkToggleBtn = document.querySelector("input[type='checkbox']");
+
+
+if (darkToggleBtn.checked === true) {
+    if (!document.body.classList.contains("dark")) {
+        document.body.classList.add("dark");
+    }
+}
+
+darkToggleBtn.addEventListener("click", () => {
+    if (darkToggleBtn.checked === true) {
+        document.body.classList.toggle("dark");
+        return
+    }
+    document.body.classList.toggle("dark");
+})
+
