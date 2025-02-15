d0c0ffe095165d160d1ef4ca23cc77ddd7f1dbe3
nidhish <nidhish1233@gmail.com>
2025-02-15 14:43:23 +0530

    update styles and dark mode

--- src/app.js
+++ src/app.js
@@ -15,3 +15,6 @@ darkToggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 })
 
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.classList.add("dark");
+}
